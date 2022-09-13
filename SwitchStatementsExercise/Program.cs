c20979366abda3e3bebf2c01c13b2cd063ae6d6a
Kaylie Phillips <kaylie.travel@gmail.com>
2022-09-13 17:04:56 -0500

    Add project files.

---
+++ SwitchStatementsExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿//Ask user for their favorite school subject
+
+
+Console.WriteLine("What is your favorite school subject");
+var userInput = (Console.ReadLine());
+
+switch (userInput)
+{
+    case "math":
+        Console.WriteLine("Math is fun and useful!");
+        break;
+    case "science":
+        Console.WriteLine("Science can be exciting, especially when you do experients!");
+        break;
+    case "english":
+        Console.WriteLine("English will be helpful for proper grammer.");
+        break;
+    case "reading":
+        Console.WriteLine("Reading is a good thing to practice.");
+        break;
+    case "PE":
+        Console.WriteLine("PE is good for your health!");
+        break;
+    default:
+        Console.WriteLine("Keep working hard in school!");
+        break;
+
+}
