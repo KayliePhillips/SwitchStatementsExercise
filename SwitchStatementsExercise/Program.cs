9b65c4c15cd1976ae9cd1b8f046e1a9dd4c53208
Kaylie Phillips <kaylie.travel@gmail.com>
2022-09-15 10:25:47 -0500

    added more cases for practice

--- SwitchStatementsExercise/Program.cs
+++ SwitchStatementsExercise/Program.cs
@@ -8,20 +8,21 @@ var userInput = (Console.ReadLine());
 switch (userInput.ToLower())
 {
     case "math":
+    case "arithmetic":
         Console.WriteLine("Math is fun and useful!");
         break;
     case "science":
         Console.WriteLine("Science can be exciting, especially when you do experients!");
         break;
     case "english":
-
         Console.WriteLine("English will be helpful for proper grammer.");
         break;
     case "reading":
         Console.WriteLine("Reading is a good thing to practice.");
         break;
-    case "PE":
-        Console.WriteLine("PE is good for your health!");
+    case "pe":
+    case "gym":
+        Console.WriteLine("Exercise is good for your health!");
         break;
     default:
         Console.WriteLine("Keep working hard in school!");
